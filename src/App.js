707fb168342d981ca8973d735f857d9efe130e03
Habib Sogbesan <h.d.sogbesan@gmail.com>
2021-08-06 22:16:33 +0100

    dice component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./assets/scss/index.scss";
+import Dice from "./components/Dice/Dice";
+import useDice from "./hooks/useDice";
 
 function App() {
+  const face = useDice(3);
   return (
+    // <Dice face={face} />
     <div className="App container">
       <header className="App-header">
         <a
