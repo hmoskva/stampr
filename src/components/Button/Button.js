0fb77b713d423830f5db90b72bfeccf28616212e
Richard Saseun <richardsaseun@gmail.com>
2021-08-07 02:10:23 +0100

    chore: test linter

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,44 +1,45 @@
-import PropTypes from "prop-types"
-import styles from "./Button.module.scss"
+import PropTypes from "prop-types";
+import styles from "./Button.module.scss";
 
 const Button = ({
-	className,
-	handleClick,
-	variant,
-	disabled,
-	block,
-	...props
+  className,
+  handleClick,
+  variant,
+  disabled,
+  block,
+  ...props
 }) => {
-	const componentClass = `btn btn-${variant}`
-	const blockStyle = {
-		display: "block",
-		width: "100%",
-	}
-	return (
-		<button
-			style={block ? blockStyle : null}
-			className={`${styles.Button} ${className} ${componentClass}`}
-			disabled={disabled}
-			onClick={handleClick}>
-			{props.label}
-		</button>
-	)
-}
+  const componentClass = `btn btn-${variant}`;
+  const blockStyle = {
+    display: "block",
+    width: "100%",
+  };
+  return (
+    <button
+      style={block ? blockStyle : null}
+      className={`${styles.Button} ${className} ${componentClass}`}
+      disabled={disabled}
+      onClick={handleClick}
+    >
+      {props.label}
+    </button>
+  );
+};
 
 Button.propTypes = {
-	label: PropTypes.string,
-	className: PropTypes.string,
-	variant: PropTypes.string,
-	block: PropTypes.bool,
-	disabled: PropTypes.bool,
-	handleClick: PropTypes.func,
-}
+  label: PropTypes.string,
+  className: PropTypes.string,
+  variant: PropTypes.string,
+  block: PropTypes.bool,
+  disabled: PropTypes.bool,
+  handleClick: PropTypes.func,
+};
 
 Button.defaultProps = {
-	variant: "primary",
-	label: "Submit",
-	block: false,
-	disabled: false,
-	className: "",
-}
-export default Button
+  variant: "primary",
+  label: "Submit",
+  block: false,
+  disabled: false,
+  className: "",
+};
+export default Button;
