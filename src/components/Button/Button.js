95c9c33fa478f785339a8a84433ca8f523b5e8be
Richard Saseun <richardsaseun@gmail.com>
2021-08-07 01:17:36 +0100

    feat: create button component and story and close #3

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,17 +1,44 @@
-import PropTypes from "prop-types";
-import styles from "./Button.module.scss";
+import PropTypes from "prop-types"
+import styles from "./Button.module.scss"
 
-const Button = (props) => {
-  return (
-    <button className={`${styles.Button} ${props.className}`}>
-      {props.label}
-    </button>
-  );
-};
+const Button = ({
+	className,
+	handleClick,
+	variant,
+	disabled,
+	block,
+	...props
+}) => {
+	const componentClass = `btn btn-${variant}`
+	const blockStyle = {
+		display: "block",
+		width: "100%",
+	}
+	return (
+		<button
+			style={block ? blockStyle : null}
+			className={`${styles.Button} ${className} ${componentClass}`}
+			disabled={disabled}
+			onClick={handleClick}>
+			{props.label}
+		</button>
+	)
+}
 
 Button.propTypes = {
-  label: PropTypes.string,
-  className: PropTypes.string,
-};
+	label: PropTypes.string,
+	className: PropTypes.string,
+	variant: PropTypes.string,
+	block: PropTypes.bool,
+	disabled: PropTypes.bool,
+	handleClick: PropTypes.func,
+}
 
-export default Button;
+Button.defaultProps = {
+	variant: "primary",
+	label: "Submit",
+	block: false,
+	disabled: false,
+	className: "",
+}
+export default Button
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,14 +1,26 @@
-import Button from "./Button";
+import Button from "./Button"
 
 export default {
-  title: "Shared/Button",
-  component: Button,
-  argTypes: {},
-};
+	title: "Shared/Button",
+	component: Button,
+	argTypes: {},
+}
 
-const Template = (args) => <Button {...args} />;
+const Template = (args) => <Button {...args} />
 
-export const General = Template.bind({});
+export const General = Template.bind({})
 General.args = {
-  label: "Button",
-};
+	label: "Submit",
+}
+
+export const Outlined = Template.bind({})
+General.args = {
+	label: "Submit",
+	variant: "outlined-secondary",
+}
+
+export const Block = Template.bind({})
+General.args = {
+	label: "Block",
+	block: true,
+}
