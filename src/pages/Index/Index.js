108ff2d10a64613554f79e70535eff41622b1be5
Habib Sogbesan <h.d.sogbesan@gmail.com>
2021-08-16 21:49:55 +0100

    minor style fixes

--- src/pages/Index/Index.js
+++ src/pages/Index/Index.js
@@ -9,12 +9,15 @@ const IndexPage = () => {
   return (
     <div className={`container position-relative ${styles.Main}`}>
       <Header links={headerLinks} />
-      <div className="row mt-3">
+      <div className="row mt-3 mb-5 mb-md-0">
         <div className="col-12">
-          <Card className={`${styles.Hero} p-5`} bgColor="#ace4da45">
+          <Card
+            className={`${styles.Hero} py-5 px-3 px-md-5`}
+            bgColor="#ace4da45"
+          >
             <div className="row justify-content-center">
-              <div className="col-6 text-center text-primary">
-                <h2 className="fw-bold mb-3">
+              <div className="col-12 col-md-6 text-center text-primary">
+                <h2 className="fw-bold mb-3 px-3 px-md-0">
                   Secure Cloud Storage & Communication
                 </h2>
                 <span className="fw-light">
@@ -27,16 +30,19 @@ const IndexPage = () => {
         </div>
       </div>
       <div className={`row justify-content-center ${styles.FooterCard}`}>
-        <div className="col-6 ">
+        <div className="col-12 col-md-6 ">
           <Card
             bgColor="#ff76762e"
-            className="p-5 d-flex justify-content-between align-items-center"
+            className="py-5 px-3 px-md-5 d-flex justify-content-between align-items-center flex-column flex-md-row"
           >
             <span className="text-primary">
               Do you want 50GB Free* storage?
             </span>
 
-            <Button label="Create An Account" className="fw-little" />
+            <Button
+              label="Create An Account"
+              className="fw-little mt-3 mt-md-0"
+            />
           </Card>
         </div>
       </div>
